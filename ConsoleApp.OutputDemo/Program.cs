c6b9eadf554695556b503b4907abe2f49e989e0a
LadyBug <Ladybug@DESKTOP-NVDB56A>
2025-02-03 18:51:16 -0800

    Add project files.

---
+++ ConsoleApp.OutputDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!, my name is Shanna");
+
+Console.WriteLine("I am from the beautiful Island of Jamaica!");
+
+Console.Write("Line 1");
+Console.Write("Line 2");
+
---
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+// there are different data type 
+
+/*
+    text - String 
+    integers - int 
+    decimal - double, float, decimal 
+    logical- bool (true or false)
+*/
+
+//string
+string name = "Lady Bug";
+
+Console.WriteLine(name);
+Console.WriteLine("I am " + name); // add one string to the other (concat)
+Console.WriteLine($"they call me  {name}"); // string interpolation
+Console.WriteLine("I was given the name {0}", name); // formatted string
+
+//integers
+int age = 25;
+int retirementYearsLeft = 40;
+int retirementAge = age + retirementYearsLeft;
+
+Console.WriteLine("My age is: " + age);
+Console.WriteLine("My retirement age is :" + retirementAge);
+
+//Boolean
+bool isRetired = false;
+Console.WriteLine("Am I retired? " + isRetired);
+
+
+
+
